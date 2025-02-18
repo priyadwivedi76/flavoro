f5e49214a4220807451747c9a4e04fb6c80c695d
Dwivedi Priya <pd7668665@gmail.com>
2025-02-18 23:34:16 +0530

    Data Added

--- src/components/CategoryMenu.jsx
+++ src/components/CategoryMenu.jsx
@@ -3,11 +3,13 @@ import React from 'react'
 const CategoryMenu = () => {
   return (
     <>
-    <div className='p-3'>
+    <div className='mx-5 pt-2'>
         <h3 className="font-semibold text-[1.3em]">Find the best Food</h3>
-        <div className='flex gap-5 p-2'>
-            <button className='px-6 mt-2 py-2 font-semibold rounded-[10px] bg-gray-200 hover:bg-green-500 hover:text-gray-50'>Hello</button>
-            <button className='px-6 mt-2 py-2 font-semibold rounded-[10px] bg-gray-200 hover:bg-green-500 hover:text-gray-50'>World</button>
+        <div className='flex gap-5 p-2 overflow-x-scroll scroll-smooth lg:overflow-x-hidden'>
+            <button className='px-3 mt-2 py-2 font-semibold rounded-[5px] bg-gray-200 hover:bg-green-500 hover:text-gray-50'>All</button>
+            <button className='px-3 mt-2 py-2 font-semibold rounded-[5px] bg-gray-200 hover:bg-green-500 hover:text-gray-50'>Breakfast</button>
+            <button className='px-3 mt-2 py-2 font-semibold rounded-[5px] bg-gray-200 hover:bg-green-500 hover:text-gray-50'>Lunch</button>
+            <button className='px-3 mt-2 py-2 font-semibold rounded-[5px] bg-gray-200 hover:bg-green-500 hover:text-gray-50'>Dinner</button>
         </div>
     </div>
     </>
