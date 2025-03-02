8ffa59cb7d81781eabcff1489049a90c0bdcb4b2
Dwivedi Priya <pd7668665@gmail.com>
2025-03-03 01:02:59 +0530

    Search-Function

--- src/components/FoodItems.jsx
+++ src/components/FoodItems.jsx
@@ -6,15 +6,19 @@ import { useSelector } from 'react-redux';
 const FoodItems = () => {
   const handleToast=(name)=>toast.success(`Added ${name} to cart`);
   const category=useSelector((state)=>state.category.category)
+  const search=useSelector((state)=>state.search.search)
   return (
     <>
     <Toaster position="top-center" reverseOrder={false}/>
     <div className='flex flex-wrap gap-8 justify-center lg:justify-start'>
       {FoodData.filter((food)=>{
         if(category === 'All'){
-          return food;
+          return food.name.toLowerCase().includes(search.toLowerCase());
         }else{
-          return category === food.category;
+          return(
+          category === food.category && 
+          food.name.toLowerCase().includes(search.toLowerCase())
+          )
         }
       }).map((foodItem)=>{
         return (
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
-
+import { useDispatch } from 'react-redux'
+import { setSearch } from '../redux/slices/SearchSlice'
 const Navbar = () => {
+  const dispatch = useDispatch();
   return (
     <>
     <div className='flex lg:flex-row md:flex-row flex-col justify-between items-center'>
@@ -8,7 +10,7 @@ const Navbar = () => {
         <h1 className='font-semibold text-[1.3em]'>{new Date().toUTCString().slice(0,16)}</h1>
         <h1 className="font-semibold text-gray-400 text-[1.3em]">Welcome to Flavoro</h1>
       </div>
-      <input type='search' placeholder='Search' className='bg-white h-10 p-5 m-5 border border-gray-300 border-2 rounded-lg w-screen-xl md:w-72 lg:w-112 outline-none'/>
+      <input type='search' placeholder='Search' onChange={(e)=>dispatch(setSearch(e.target.value))} className='bg-white h-10 p-5 m-5 border border-gray-300 border-2 rounded-lg w-screen-xl md:w-72 lg:w-112 outline-none'/>
     </div>
     <hr className='w-full h-[0.5px] bg-black-100 opacity-50'/>
     </>
