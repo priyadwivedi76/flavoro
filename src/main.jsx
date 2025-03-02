80c4ca7030f296e92042eecb5116a8780ac4a8da
Dwivedi Priya <pd7668665@gmail.com>
2025-03-02 16:12:52 +0530

    redux

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import { Provider } from 'react-redux'
+import Store from './redux/Store.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <StrictMode>
-    <App />
+    <Provider store={Store}>
+      <App />
+    </Provider>
   </StrictMode>
   </BrowserRouter>,
 )
